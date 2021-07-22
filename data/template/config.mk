965f76334806a6614d61ca9da6120016ac269364
Lupino <lmjubuntu@gmail.com>
2021-07-22 10:18:20 +0800

    add template Makefile

---
+++ data/template/config.mk
@@ -0,0 +1,15 @@
+{{={= =}=}}
+ARDUINO_CLI ?= arduino-cli
+BOARD_TAG ?= arduino:avr:pro
+BUILD_PATH ?= $(CURDIR)/arduino_build
+
+{=# app =}
+KEY = {= key =}
+PROJECT_NAME = {= name =}
+{=/ app =}
+
+ifeq ($(BOARD_TAG),rp2040:rp2040:rpipico)
+DIST_APP = $(PROJECT_NAME).uf2
+else
+DIST_APP = $(PROJECT_NAME).hex
+endif
