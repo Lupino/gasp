132deed0321694c408cafa940bad1a58157bc404
Lupino <lmjubuntu@gmail.com>
2021-07-28 16:02:55 +0800

    update template name

--- data/template/app/app.ino
+++ data/template/app/app.ino
@@ -18,10 +18,10 @@
 {=/ raws =}
 {=# vars =}
 {=# has_value =}
-{= type =} {= name =} = {=& value =};
+{= type =} {=& name =} = {=& value =};
 {=/ has_value =}
 {=^ has_value =}
-{= type =} {= name =};
+{= type =} {=& name =};
 {=/ has_value =}
 {=/ vars =}
 
