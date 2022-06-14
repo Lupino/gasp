a933e44dfbe7dc59b43de22f559e6e627a50e009
Lupino <lmjubuntu@gmail.com>
2022-06-14 23:23:29 +0800

    EEPROM to STROAGE

--- data/template/app/app.ino
+++ data/template/app/app.ino
@@ -25,15 +25,6 @@
 {=/ has_value =}
 {=/ vars =}
 
-{=# has_app =}
-#ifdef ARDUINO_ARCH_RP2040
-#define EEPROM_NEED_COMMIT
-#ifndef EEPROM_SIZE
-#define EEPROM_SIZE 1024
-#endif
-#endif
-
-{=/ has_app =}
 {=# functions =}
 {= type =} {= name =}({=# argv =}{= type =} {= name =}{=^ last =}, {=/ last =}{=/ argv =});
 {=/ functions =}
