03100176be06e76a508ffedf1e5643f27dda3e70
Lupino <lmjubuntu@gmail.com>
2025-08-22 10:15:28 +0800

    add esp32

--- data/template/stage2/config.mk
+++ data/template/stage2/config.mk
@@ -14,10 +14,12 @@ TARGET_INO = stage2.ino
 
 ifeq ($(findstring rp2040:rp2040, $(BOARD_TAG)),rp2040:rp2040)
 TARGET_APP = $(PROJECT_NAME).uf2
+else ifeq ($(findstring esp32:esp32, $(BOARD_TAG)),esp32:esp32)
+TARGET_APP = $(PROJECT_NAME).merged.bin
 else
 TARGET_APP = $(PROJECT_NAME).hex
 endif
 
 
-ARDUINO_DATA = $(HOME)/Library/Arduino15
-ARDUINO_LIB_PATH = $(HOME)/Documents/Arduino/libraries
+ARDUINO_DATA = $(shell $(ARDUINO_CLI) config get directories.data)
+ARDUINO_LIB_PATH = $(shell  $(ARDUINO_CLI) config get directories.user)/libraries
